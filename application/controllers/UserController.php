feb41fc584eb59fb67ab70afc14eee088d74a6b8
COOL_CAL <grin0330@gmail.com>
2022-07-18 10:00:10 +0900

    Shop

---
+++ application/controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace application\controllers;
+
+    class UserController extends Controller {
+        public function signUp() {
+            $json = getJson();
+            $result = $this->model->signUp($json);
+            if($result) {
+                $this->flash(_LOGINUSER, $result);
+                return [_RESULT => 1];
+            }
+            return [_RESULT => 0];
+        }
+    }
